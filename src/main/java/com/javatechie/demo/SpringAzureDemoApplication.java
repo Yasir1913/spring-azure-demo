01afb0f402d9bb543090fc37400e23a6f33bd810
Yasir ajmal <yasir.ajmal@cynoteck.com>
2024-12-26 12:50:27 +0530

    second commit

--- src/main/java/com/javatechie/demo/SpringAzureDemoApplication.java
+++ src/main/java/com/javatechie/demo/SpringAzureDemoApplication.java
@@ -11,7 +11,7 @@ public class SpringAzureDemoApplication {
 
 	@GetMapping("/message")
 	public String message(){
-		return "Congrats! you app deployed successfully.....";
+		return "Congrats! your application deployed successfully on azure portal!!...";
 	}
 
 	public static void main(String[] args) {
